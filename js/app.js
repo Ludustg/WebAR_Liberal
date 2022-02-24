bf9d207e9b1a3747b924e6a24c9687a65a76f273
italo <italoludus@gmail.com>
2022-02-24 15:19:46 -0300

    Update 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR_Liberal.json");
 let WebXR;
 window.ARWT = {}
 
